33680d140b5ed67787c47f850dbec806a20bf2bc
Ryan McNair <ryan.mcnair@gmail.com>
2020-12-03 09:12:44 -0600

    initial setup complete

---
+++ src/App/Metronome.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Metronome.scss';
+
+class App extends React.Component {
+  render() {
+    return <div className="metronome">hi</div>;
+  }
+}
+
+export default App;
